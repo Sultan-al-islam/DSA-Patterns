d6076e1ef2891838822e37e82f7bfc0dfea3c9f2
Sultan Al Islam <sultan57mu@gmail.com>
2026-09-02 07:07:30 +0600

    Day1v2

--- TwoPointers/TwoSumII_InputArrayIsSorted.cpp
+++ TwoPointers/TwoSumII_InputArrayIsSorted.cpp
@@ -1,3 +1,40 @@
+// Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+// Return the indices of the two numbers index1 and index2, each incremented by one, as an integer array [index1, index2] of length 2.
+
+// The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+// Your solution must use only constant extra space.
+
+ 
+
+// Example 1:
+
+// Input: numbers = [2,7,11,15], target = 9
+// Output: [1,2]
+// Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+// Example 2:
+
+// Input: numbers = [2,3,4], target = 6
+// Output: [1,3]
+// Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
+// Example 3:
+
+// Input: numbers = [-1,0], target = -1
+// Output: [1,2]
+// Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
+ 
+
+// Constraints:
+
+// 2 <= numbers.length <= 3 * 104
+// -1000 <= numbers[i] <= 1000
+// numbers is sorted in non-decreasing order.
+// -1000 <= target <= 1000
+// The tests are generated such that there is exactly one solution.
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -29,23 +66,4 @@ vector<int> twoSum(vector<int> &num, int target)
     return {a, b};
 }
 
-int main()
-{
 
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-
-
-    vi v{2,7,11,15};
-    int t=9;
-
-
-    vi ans=twoSum(v,t);
-    for(auto u:ans){
-        cout<<u<<" ";
-
-    }
-
-    return 0;
-}
